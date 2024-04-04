68c57fe82fd901229cf4ac6d1b6e09a31f307866
L4DNO <ves.artem2005@gmail.com>
2024-04-04 23:59:04 +0300

    Add 503 error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Техобслуживание!</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-black text-white">
+    <div class="container mx-auto">
+        <h1 class="mt-16 text-center text-4xl font-bold">Сайт на техобслуживании, приносим прощения за неудобства.</h1>
+        <p class="text-lightgray text-center mt-2 font-semibold text-2xl">Обещаем, мы скоро откроемся!</p>
+    </div>
+</body>
+</html>
