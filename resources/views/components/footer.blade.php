3431fb0a00660129d86e9db78ce9417e9598aea3
L4DNO <ves.artem2005@gmail.com>
2024-04-04 19:58:25 +0300

    change link in footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="w-full text-xl mt-16">
     <div class="w-4/5 2xl:w-3/5 mx-auto flex flex-col md:flex-row justify-between border-t-4 border-t-light-100 text-light-100 py-6 max-md:gap-8">
         <div class="md:basis-1/3">
-            <p class="max-xl:text-center md:max-w-[20ch]">Design by <a href="https://t.me/VIInig" class="underline underline-offset-4  hover:bg-lightgray/50 transition-colors duration-350">VinigR3D</a> and <a href="https://t.me/Fredderik0" class="underline underline-offset-4  hover:bg-lightgray/50 transition-colors duration-350">Humusus_</a>. Developed by
+            <p class="max-xl:text-center md:max-w-[20ch]">Design by <a href="http://tg.me/SG_DESGN" class="underline underline-offset-4  hover:bg-lightgray/50 transition-colors duration-350">VinigR3D</a> and <a href="https://t.me/Fredderik0" class="underline underline-offset-4  hover:bg-lightgray/50 transition-colors duration-350">Humusus_</a>. Developed by
                 <a class="underline underline-offset-4 hover:bg-lightgray/50 transition-colors duration-350" href="https://t.me/L4DN00">L4DNO</a>.
             </p>
         </div>
